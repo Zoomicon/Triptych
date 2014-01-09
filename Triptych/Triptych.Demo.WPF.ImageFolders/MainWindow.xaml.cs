76166a1b44249ba386adb5d7edde15766f6abd87
birbilis <birbilis@kagi.com>
2014-01-09 21:48:03 +0200

    Timed transitions added to WPF demo, made maximized with no titlebar and resize border

--- Triptych/Triptych.Demo.WPF.ImageFolders/MainWindow.xaml.cs
+++ Triptych/Triptych.Demo.WPF.ImageFolders/MainWindow.xaml.cs
@@ -4,15 +4,16 @@ Filename: Triptych.Demo.Silverlight.Webcam\MainPage.xaml.cs
 Version: 20140109
 */
 
-using System.Linq;
-using System.Text.RegularExpressions;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace Triptych.Demo.WPF.ImageFolders
 {
@@ -21,14 +22,25 @@ namespace Triptych.Demo.WPF.ImageFolders
   /// </summary>
   public partial class MainWindow : Window
   {
-  
+
+    #region --- Constants ---
+
+    const string BASE_FOLDER = "Photos";
+    const string DEFAULT_FOLDER = "FruitBowl";
+    const int DELAY_CENTER = 10; //sec
+    const int DELAY_SIDES = 20; //sec
+
+    #endregion
+    
     #region --- Fields ---
 
     bool initialized = false;
     List<ImageSource> imageSources = new List<ImageSource>();
     ImageBrush imageBrushLeft, imageBrushCenter, imageBrushRight;
     int imageSourceLeftIndex, imageSourceCenterIndex, imageSourceRightIndex;
-  
+    DispatcherTimer timerCenter, timerSides;
+    Vision vision = new Vision();
+    
     #endregion
 
     #region --- Methods ---
@@ -38,60 +50,79 @@ namespace Triptych.Demo.WPF.ImageFolders
       InitializeComponent();
       App.Current.MainWindow.SizeChanged += (s, e) =>
       {
-        Init();
+        if (!initialized)
+        {
+          Init();
+          Start();
+        }
       };
+
     }
-    
+     
     public void Init()
     {
-      try
-      {
-        var imageFilenames = Directory.EnumerateFiles(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory,"images")).Where(file => Regex.IsMatch(file, @"^.+\.(jpg|png|bmp)$"));
-        foreach (string filename in imageFilenames)
-          imageSources.Add(new BitmapImage(new Uri(filename, UriKind.RelativeOrAbsolute)));
-      }
-      catch {
-        Clear();
-        return;
-      }
-
-      imageSourceLeftIndex = 0 % imageSources.Count;
-      imageSourceCenterIndex = 1 % imageSources.Count;
-      imageSourceRightIndex = 2 % imageSources.Count;
-
-      imageBrushLeft = new ImageBrush() { Stretch = Stretch.UniformToFill };
-      imageBrushCenter = new ImageBrush() { Stretch = Stretch.UniformToFill };
-      imageBrushRight = new ImageBrush() { Stretch = Stretch.UniformToFill };
-
-      Vision v = new Vision();
-      v.Start(new WindowInteropHelper(this).Handle, 100, 100); //(int)Width, (int)Height
+      InitImages();
+      InitAnimation();
 
       if (!initialized)
       {
-        Update();
+        UpdateImages();
         initialized = true;
       }
     }
 
-    public void Update()
+    #region Media
+
+
+    private void InitImages()
+    {
+      LoadImages(DEFAULT_FOLDER);
+      InitBrushes();
+    }
+
+    private void LoadImages(string folder)
+    {
+      try
+      {
+        var imageFilenames = Directory.EnumerateFiles(Path.Combine(new String[]{System.AppDomain.CurrentDomain.BaseDirectory, BASE_FOLDER, folder})).Where(file => Regex.IsMatch(file, @"^.+\.(jpg|png|bmp)$"));
+        foreach (string filename in imageFilenames)
+          imageSources.Add(new BitmapImage(new Uri(filename, UriKind.RelativeOrAbsolute)));
+      }
+      catch
+      {
+        ClearImages();
+        return;
+      }
+    }
+
+    private void InitBrushes()
+    {
+      imageSourceLeftIndex = imageSourceRightIndex = imageSourceCenterIndex;
+
+      imageBrushLeft = new ImageBrush() { Stretch = Stretch.UniformToFill };
+      imageBrushCenter = new ImageBrush() { Stretch = Stretch.UniformToFill };
+      imageBrushRight = imageBrushLeft; //new ImageBrush() { Stretch = Stretch.UniformToFill };
+
+      ViewportCenter.Fill = imageBrushCenter;
+      ViewportLeft.Fill = imageBrushLeft;
+      ViewportRight.Fill = imageBrushRight;
+    }
+
+    public void UpdateImages()
     {
       try
       {
         imageBrushLeft.ImageSource = imageSources[imageSourceLeftIndex];
-        ViewportLeft.Fill = imageBrushLeft;
-
         imageBrushCenter.ImageSource = imageSources[imageSourceCenterIndex];
-        ViewportCenter.Fill = imageBrushCenter;
-
         imageBrushRight.ImageSource = imageSources[imageSourceRightIndex];
-        ViewportRight.Fill = imageBrushRight;
       }
-      catch {
+      catch
+      {
         //NOP
       }
     }
 
-    public void Clear()
+    public void ClearImages()
     {
       try
       {
@@ -104,48 +135,114 @@ namespace Triptych.Demo.WPF.ImageFolders
         //NOP
       }
     }
-    
+
+    #endregion
+
+    #region Animation
+
+    private void InitAnimation()
+    {
+      timerCenter = new System.Windows.Threading.DispatcherTimer();
+      timerCenter.Tick += new EventHandler(TimerCenter_Tick);
+      timerCenter.Interval = new TimeSpan(0, 0, DELAY_CENTER);
+
+      timerSides = new System.Windows.Threading.DispatcherTimer();
+      timerSides.Tick += new EventHandler(TimerSides_Tick);
+      timerSides.Interval = new TimeSpan(0, 0, DELAY_SIDES);
+    }
+
+    private void StartAnimation()
+    {
+      timerCenter.Start();
+      timerSides.Start();
+    }
+
+    private void StopAnimation()
+    {
+      timerCenter.Stop();
+      timerSides.Stop();
+    }
+
     public void PreviousLeft()
     {
       imageSourceLeftIndex = CoerceIndex(imageSourceLeftIndex - 1);
-      Update();
+      UpdateImages();
     }
 
     public void NextLeft()
     {
       imageSourceLeftIndex = CoerceIndex(imageSourceLeftIndex + 1);
-      Update();
+      UpdateImages();
     }
 
     public void PreviousCenter()
     {
       imageSourceCenterIndex = CoerceIndex(imageSourceCenterIndex - 1);
-      Update();
+      UpdateImages();
     }
 
     public void NextCenter()
     {
       imageSourceCenterIndex = CoerceIndex(imageSourceCenterIndex + 1);
-      Update();
+      UpdateImages();
     }
 
     public void CenterToLeftRight()
     {
       imageSourceRightIndex = imageSourceLeftIndex = imageSourceCenterIndex;
-      Update();
+      UpdateImages();
     }
-    
+
     public void PreviousRight()
     {
       imageSourceRightIndex = CoerceIndex(imageSourceRightIndex - 1);
-      Update();
+      UpdateImages();
     }
 
     public void NextRight()
     {
       imageSourceRightIndex = CoerceIndex(imageSourceRightIndex + 1);
-      Update();
+      UpdateImages();
     }
+    
+    public void NextLeftRight()
+    {
+      imageSourceLeftIndex = CoerceIndex(imageSourceLeftIndex + 1);
+      imageSourceRightIndex = CoerceIndex(imageSourceRightIndex + 1);
+      UpdateImages();
+    }
+
+    #endregion
+
+    #region Vision
+
+    public void StartVision()
+    {
+      vision.Start(new WindowInteropHelper(this).Handle, 64, 48); //(int)Width, (int)Height
+    }
+
+    public void StopVision()
+    {
+      vision.Stop();
+    }
+
+    #endregion
+
+    public void Start()
+    {
+      UpdateImages();
+      StartAnimation();
+      StartVision();
+    }
+
+    public void Stop()
+    {
+      StopAnimation();
+      StopVision();
+      ClearImages();
+    }
+
+    #region Helpers
 
     public int CoerceIndex(int index)
     {
@@ -161,10 +258,28 @@ namespace Triptych.Demo.WPF.ImageFolders
       else
         return index;
     }
-    
+
+    #endregion
+
     #endregion
 
     #region --- Events ---
+
+    #region Timers
+
+    private void TimerCenter_Tick(object sender, EventArgs e)
+    {
+      NextCenter();
+    }
+
+    private void TimerSides_Tick(object sender, EventArgs e)
+    {
+      NextLeftRight(); //do in a single step
+    }
+    
+    #endregion
+
+    #region Mouse
 
     private void ViewportLeft_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
@@ -202,7 +317,9 @@ namespace Triptych.Demo.WPF.ImageFolders
     }
 
     #endregion
- 
+
+    #endregion
+
   }
 }
 
